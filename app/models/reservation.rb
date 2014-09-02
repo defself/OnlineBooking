5994d1716e58de77301d8b49465ee60e5e57e3e3
seniorihor <seniorigor@gmail.com>
2014-09-02 14:56:19 +0300

    Have fun with borders & gradients

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ActiveRecord::Base
   validates_presence_of :start_time, :end_time, :table_id
   validates :start_time, :end_time, overlap: { scope: 'table_id',
-                                               message_content: 'overlaps with Tables other reservation.' }
+                                               message_content: 'overlaps with Tables other reservation' }
 
   belongs_to :table
 end
